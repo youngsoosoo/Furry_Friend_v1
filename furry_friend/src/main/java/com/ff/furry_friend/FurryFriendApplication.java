e47ed970d4c4f76b855b3a0297b02abf1c2c24fa
youngsoosoo <lovepys01@naver.com>
2022-07-27 18:05:45 +0900

    초기설정

---
+++ furry_friend/src/main/java/com/ff/furry_friend/FurryFriendApplication.java
@@ -0,0 +1,13 @@
+package com.ff.furry_friend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurryFriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurryFriendApplication.class, args);
+	}
+
+}
---
+++ furry_friend/src/test/java/com/ff/furry_friend/FurryFriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ff.furry_friend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurryFriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
