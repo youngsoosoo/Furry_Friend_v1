d8dbc602395679076b55c95697849243434c8dd2
youngsoosoo <lovepys01@naver.com>
2022-11-21 17:20:38 +0900

    안 되면 이거 TRIM 제거

--- furry_friend/src/main/java/com/ff/furry_friend/repository/MemoryProductRepository.java
+++ furry_friend/src/main/java/com/ff/furry_friend/repository/MemoryProductRepository.java
@@ -32,7 +32,7 @@ public class MemoryProductRepository implements ProductRepository {
 
     @Override
     public List<product> findPartName(String name){
-        List<product> result =  em.createQuery("select p from product p where p.pro_name LIKE concat('%', :pro_name, '%')", product.class)
+        List<product> result =  em.createQuery("select p from product p where p.pro_name LIKE TRIM(concat('%', :pro_name, '%'))", product.class)
                 .setParameter("pro_name", name)
                 .getResultList();
         return result;
