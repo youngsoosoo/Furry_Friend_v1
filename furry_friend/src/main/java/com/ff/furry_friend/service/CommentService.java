4d0c848ec9710b36b014acbfa139cbfab3e1f7ea
youngsoosoo <lovepys01@naver.com>
2023-03-10 11:55:30 +0900

    버전 수정

--- furry_friend/src/main/java/com/ff/furry_friend/service/CommentService.java
+++ furry_friend/src/main/java/com/ff/furry_friend/service/CommentService.java
@@ -5,6 +5,7 @@ import com.ff.furry_friend.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
